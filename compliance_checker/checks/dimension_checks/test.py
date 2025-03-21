aff4fe2f9abfcc38a328db09befab2f6c23c4f5a
Ayoub Nachite <nachite66@gmail.com>
2025-03-21 14:43:59 +0100

    add checks folders

---
+++ compliance_checker/checks/dimension_checks/test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Define the sheets as dataframes
+global_attributes = pd.DataFrame({
+    "Attribute": [
+        "Conventions", "title", "institution", "source", "history", "project_id",
+        "activity_id", "experiment_id", "parent_experiment_id", "grid_label",
+        "nominal_resolution", "geospatial_lat_min", "license"
+    ],
+
+
