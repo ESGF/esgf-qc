7a2a96ea7c0d5e7ba2fcdffa7a9e5e8718dee5b0
Joakim Low <joakim.low@smhi.se>
2025-03-25 09:16:45 +0100

    Updated test to check resulting messages

--- compliance_checker/tests/checks/variable_checks/test_check_variable_existence.py
+++ compliance_checker/tests/checks/variable_checks/test_check_variable_existence.py
@@ -3,21 +3,22 @@ import pytest
 from compliance_checker.checks.variable_checks.check_variable_existence import check_variable_existence
 
 TEST_CHECK_VARIABLE_EXISTENCE = [
-    ("variable_exists", "checks_test_ds", "time", (1, 1)),
-    ("variable_does_not_exist", "checks_test_ds", "novar", (0, 1)),
+    ("variable_exists", "checks_test_ds", "time", [], (1, 1)),
+    ("variable_does_not_exist", "checks_test_ds", "novar", ["Variable 'novar' is missing"], (0, 1)),
 ]
 
 
 @pytest.mark.parametrize(
-    "test_id,ds_fixture,var_name,expected_value",
+    "test_id,ds_fixture,var_name,expected_msgs,expected_value",
     TEST_CHECK_VARIABLE_EXISTENCE,
     ids=[t[0] for t in TEST_CHECK_VARIABLE_EXISTENCE],
 )
-def test_check_variable_existence(request, test_id, ds_fixture, var_name, expected_value):
+def test_check_variable_existence(request, test_id, ds_fixture, var_name, expected_msgs, expected_value):
     ds = request.getfixturevalue(ds_fixture)
     results = check_variable_existence(ds, var_name, check_id=test_id)
     assert len(results) == 1
     assert results[0].name.endswith("Variable Existence Check")
+    assert results[0].msgs == expected_msgs
     assert results[0].value == expected_value
     if results[0].value[0] != results[0].value[1]:
         assert test_id in results[0].name
